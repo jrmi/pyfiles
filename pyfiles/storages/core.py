baadc707edf806780784590ba74b2380b637cad4
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2019-03-03 17:42:51 +0100

    Fix #5 Add sync version of all method

--- pyfiles/storages/core.py
+++ pyfiles/storages/core.py
@@ -1,8 +1,36 @@
+import asyncio
 
-class Storage: # TODO necessary ?
+
+class Storage:  # TODO necessary ?
     async def search(self, namespace, filename, version):
         raise NotImplementedError()
+
     async def versions(self, namespace, filename):
         raise NotImplementedError()
+
     async def store(self, stream, namespace, filename):
         raise NotImplementedError()
+
+    async def delete(self, stream, namespace, filename):
+        raise NotImplementedError()
+
+    # Synced version of previous method
+    def search_sync(self, namespace, filename, version):
+        return asyncio.get_event_loop().run_until_complete(
+            self.search(namespace, filename, version)
+        )
+
+    def versions_sync(self, namespace, filename):
+        return asyncio.get_event_loop().run_until_complete(
+            self.versions(namespace, filename)
+        )
+
+    def store_sync(self, stream, namespace, filename):
+        return asyncio.get_event_loop().run_until_complete(
+            self.store(stream, namespace, filename)
+        )
+
+    def delete_sync(self, namespace, filename, version):
+        return asyncio.get_event_loop().run_until_complete(
+            self.delete(namespace, filename, version)
+        )
