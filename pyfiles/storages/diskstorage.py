a15feb98fc579d2e26f8148806e9fa3de06deb8d
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2019-03-03 17:24:39 +0100

    Add delete for diskstorage

--- pyfiles/storages/diskstorage.py
+++ pyfiles/storages/diskstorage.py
@@ -2,13 +2,14 @@ import os
 
 from pyfiles.storages.core import Storage
 
+
 class DiskStorage(Storage):
     def __init__(self, basepath, base_url):
         self.base = os.path.realpath(basepath)
         self.base_url = base_url
 
-    async def search(self, namespace, filename, version='latest'):
-        basename = os.path.join(self.base, *namespace.split('.'))
+    async def search(self, namespace, filename, version="latest"):
+        basename = os.path.join(self.base, *namespace.split("."))
 
         # TODO Should we awaitable
         all_files = sorted(os.listdir(basename))
@@ -16,7 +17,7 @@ class DiskStorage(Storage):
         # TODO add regex match for YYYY_MM_DD-VV__<filename>
         filelist = [f for f in all_files if f.endswith(filename)]
 
-        if version != 'latest':
+        if version != "latest":
             filelist = [f for f in filelist if f.startswith(version)]
 
         if not filelist:
@@ -28,32 +29,37 @@ class DiskStorage(Storage):
         filepath = os.path.join(basename, selected_file)
 
         return {
-            'version': selected_file.split('__')[0],
-            'url': f'{self.base_url}{filepath}'
+            "version": selected_file.split("__")[0],
+            "url": f"{self.base_url}{filepath}",
         }
 
     async def versions(self, namespace, filename):
-        basename = os.path.join(self.base, *namespace.split('.'))
+        basename = os.path.join(self.base, *namespace.split("."))
 
         # TODO Should we awaitable
         all_files = sorted(os.listdir(basename))
 
-        versionlist = [f.split('__')[0] for f in all_files if f.endswith(filename)]
+        versionlist = [f.split("__")[0] for f in all_files if f.endswith(filename)]
 
         return versionlist
 
     async def store(self, stream, namespace, filename, version):
-        basename = os.path.join(self.base, *namespace.split('.'))
+        basename = os.path.join(self.base, *namespace.split("."))
 
         try:
             os.makedirs(basename)
         except OSError:
             pass
 
-        filepath = os.path.join(basename, f'{version}__{filename}')
+        filepath = os.path.join(basename, f"{version}__{filename}")
 
         # TODO make it async
-        with open(filepath, 'wb') as fout:
+        with open(filepath, "wb") as fout:
             fout.write(stream.read())
 
+    async def delete(self, namespace, filename, version):
+        basename = os.path.join(self.base, *namespace.split("."))
+        filepath = os.path.join(basename, f"{version}__{filename}")
 
+        # TODO make it async
+        os.remove(filepath)
--- pyfiles/storages/s3storage.py
+++ pyfiles/storages/s3storage.py
@@ -4,9 +4,16 @@ import botocore
 
 from pyfiles.storages.core import Storage
 
-class S3Storage(Storage):
 
-    def __init__(self, access_key=None, secret_key=None, endpoint_url=None, region_name=None, bucket_name=""):
+class S3Storage(Storage):
+    def __init__(
+        self,
+        access_key=None,
+        secret_key=None,
+        endpoint_url=None,
+        region_name=None,
+        bucket_name="",
+    ):
         self.access_key = access_key
         self.secret_key = secret_key
         self.endpoint_url = endpoint_url
@@ -14,27 +21,26 @@ class S3Storage(Storage):
         self.bucket_name = bucket_name
 
     def get_client(self):
-        if not hasattr(self, '_s3'):
+        if not hasattr(self, "_s3"):
             self._s3 = boto3.resource(
-                's3',
+                "s3",
                 aws_access_key_id=self.access_key,
                 aws_secret_access_key=self.secret_key,
                 endpoint_url=self.endpoint_url,
-                region_name=self.region_name
+                region_name=self.region_name,
             )
 
         return self._s3
 
     def _generate_temp_url(self, obj):
         return self.get_client().meta.client.generate_presigned_url(
-            'get_object',
-            Params={'Bucket': self.bucket_name, 'Key': obj.key}
+            "get_object", Params={"Bucket": self.bucket_name, "Key": obj.key}
         )
 
-    async def search(self, namespace, filename, version='latest'):
+    async def search(self, namespace, filename, version="latest"):
         s3 = self.get_client()
 
-        if version == 'latest':
+        if version == "latest":
             version_prefix = ""
         else:
             version_prefix = version
@@ -42,13 +48,17 @@ class S3Storage(Storage):
         bucket = s3.Bucket(self.bucket_name)
 
         filelist = []
-        for obj in bucket.objects.filter(Prefix="/".join(namespace.split('.'))):
+        for obj in bucket.objects.filter(Prefix="/".join(namespace.split("."))):
 
-            if obj.key.endswith(filename): # Filter only wanted filename
+            if obj.key.endswith(filename):  # Filter only wanted filename
                 full_obj = s3.Object(self.bucket_name, obj.key)
-                obj_version = full_obj.metadata.get('version') # Extract version from metadata
+                obj_version = full_obj.metadata.get(
+                    "version"
+                )  # Extract version from metadata
 
-                if obj_version and obj_version.startswith(version_prefix): # Filter version
+                if obj_version and obj_version.startswith(
+                    version_prefix
+                ):  # Filter version
                     filelist.append((obj_version, full_obj))
 
         if not filelist:
@@ -57,8 +67,8 @@ class S3Storage(Storage):
         selected_file = sorted(filelist, key=lambda x: x[0])[-1][1]
 
         return {
-            'version': selected_file.metadata['version'],
-            'url': self._generate_temp_url(selected_file)
+            "version": selected_file.metadata["version"],
+            "url": self._generate_temp_url(selected_file),
         }
 
     async def versions(self, namespace, filename):
@@ -67,14 +77,15 @@ class S3Storage(Storage):
         bucket = s3.Bucket(self.bucket_name)
 
         filelist = []
-        for obj in bucket.objects.filter(Prefix="/".join(namespace.split('.'))):
+        for obj in bucket.objects.filter(Prefix="/".join(namespace.split("."))):
 
-            if obj.key.endswith(filename): # Filter only wanted filename
+            if obj.key.endswith(filename):  # Filter only wanted filename
                 full_obj = s3.Object(self.bucket_name, obj.key)
-                obj_version = full_obj.metadata.get('version') # Extract version from metadata
+                obj_version = full_obj.metadata.get(
+                    "version"
+                )  # Extract version from metadata
 
                 filelist.append((obj_version, full_obj))
-
 
         return [o[0] for o in filelist]
 
@@ -82,22 +93,18 @@ class S3Storage(Storage):
         s3 = self.get_client()
 
         # FIXME can we use os.path join here ?
-        fname = "/".join(namespace.split('.')) + f'/{version}/{filename}'
+        fname = "/".join(namespace.split(".")) + f"/{version}/{filename}"
 
         # TODO make it async
         s3.Bucket(self.bucket_name).put_object(
-            Key=fname,
-            Body=stream,
-            Metadata={
-                'version': version,
-            }
+            Key=fname, Body=stream, Metadata={"version": version}
         )
 
     async def delete(self, namespace, filename, version):
         s3 = self.get_client()
 
         # FIXME can we use os.path join here ?
-        fname = "/".join(namespace.split('.')) + f'/{version}/{filename}'
+        fname = "/".join(namespace.split(".")) + f"/{version}/{filename}"
 
         # TODO make it async
         s3.Object(self.bucket_name, fname).delete()
